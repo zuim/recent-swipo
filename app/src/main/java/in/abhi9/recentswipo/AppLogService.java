f5aebc9acc8093cb1187b76bc319ac4f16371bbb
Felix Klenner <info@zuim.de>
2019-01-21 20:35:57 +0100

    v 2.0
    fix for pie
    add loop
    add icon
    change animation

--- app/src/main/java/in/abhi9/recentswipo/AppLogService.java
+++ app/src/main/java/in/abhi9/recentswipo/AppLogService.java
@@ -5,9 +5,11 @@ import android.app.ActivityOptions;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -23,6 +25,7 @@ public class AppLogService extends Service {
     private List<ActivityManager.RecentTaskInfo> recents = new ArrayList<>();
     private Integer next = 0;
     private ActivityManager.RecentTaskInfo launcherTask;
+	private SharedPreferences prefs;
 
     public static AppLogService getServiceObject() {
         return self;
@@ -36,6 +39,7 @@ public class AppLogService extends Service {
     @Override
     public void onCreate() {
         mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     @Override
@@ -61,22 +65,28 @@ public class AppLogService extends Service {
 
         if (Objects.equals(getCurrentActivity(), getHomeLauncher())) {
             next = 0;
-            Log.d(TAG, "Currently on Home. Launching 1st app");
+            Log.d(TAG, "SL - Currently on Home. Launching 1st app");
         }
 
         if ((nextApp = getNextActiveApp("forward")) == null) {
             Log.d(TAG, "SL - No next active app found");
-            return;
+            if(prefs.getBoolean("loop", false)){
+            	next = -1;
+            	nextApp = launcherTask;
+            }
+            else {
+	            return;
+            }
         }
 
-        Log.d(TAG, "App to Launch:" + nextApp.baseIntent.getComponent().getPackageName());
+        Log.d(TAG, "SL - App to Launch:" + nextApp.baseActivity.getPackageName());
 
         mActivityManager.moveTaskToFront(
                 nextApp.id,
                 ActivityManager.MOVE_TASK_NO_USER_ACTION,
                 getStartActivityOption("left").toBundle()
         );
-        lastLaunchedBySwipe = nextApp.baseIntent.getComponent().getPackageName();
+        lastLaunchedBySwipe = nextApp.baseActivity.getPackageName();
         next++;
         Log.d(TAG, "SL - Next: " + next);
     }
@@ -89,11 +99,18 @@ public class AppLogService extends Service {
         if (Objects.equals(getCurrentActivity(), getHomeLauncher())) {
             next = 0;
             Log.d(TAG, "SR - Currently on Home");
-            return;
+	        if (prefs.getBoolean("loop", false))
+	        {
+	        	next = recents.size();
+		        nextApp = recents.get(recents.size() - 1);
+	        }
+	        else
+	        {
+		        return;
+	        }
         }
-
-        if ((nextApp = getNextActiveApp("backward")) == null) {
-            Log.d(TAG, "SL - No previous active app found. Going home.");
+        else if ((nextApp = getNextActiveApp("backward")) == null) {
+            Log.d(TAG, "SR - No previous active app found. Going home.");
             mActivityManager.moveTaskToFront(
                     launcherTask.id,
                     ActivityManager.MOVE_TASK_NO_USER_ACTION,
@@ -103,16 +120,16 @@ public class AppLogService extends Service {
             return;
         }
 
-        Log.d(TAG, "App to Launch:" + nextApp.baseIntent.getComponent().getPackageName());
+        Log.d(TAG, "SR - App to Launch:" + nextApp.baseActivity.getPackageName());
 
         mActivityManager.moveTaskToFront(
                 nextApp.id,
                 ActivityManager.MOVE_TASK_NO_USER_ACTION,
                 getStartActivityOption("right").toBundle()
         );
-        lastLaunchedBySwipe = nextApp.baseIntent.getComponent().getPackageName();
+        lastLaunchedBySwipe = nextApp.baseActivity.getPackageName();
         next--;
-        Log.d(TAG, "SL - Next: " + next);
+        Log.d(TAG, "SR - Next: " + next);
     }
 
     private void initList() {
@@ -136,16 +153,17 @@ public class AppLogService extends Service {
 
         if (Objects.equals(direction, "forward")) {
             for (Integer i = this.next + 1; i < this.recents.size(); i++) {
-                if (ids.contains(this.recents.get(i).id)) return this.recents.get(i);
+                if (this.recents.get(i) != null && ids.contains(this.recents.get(i).id)) return this.recents.get(i);
             }
         } else {
             for (Integer i = this.next - 1; i > -1; i--) {
-                if (ids.contains(this.recents.get(i).id)) return this.recents.get(i);
+                if (this.recents.get(i) != null && ids.contains(this.recents.get(i).id)) return this.recents.get(i);
             }
         }
 
         return null;
     }
+
 
     private List<ActivityManager.RecentTaskInfo> getRecents() {
 
@@ -157,16 +175,31 @@ public class AppLogService extends Service {
                         ActivityManager.RECENT_IGNORE_UNAVAILABLE | 4 |
                                 ActivityManager.RECENT_WITH_EXCLUDED);
 
+		//get launcher, because it's not included in getrecenttasks
+	    for (ActivityManager.RunningTaskInfo rt : mActivityManager.getRunningTasks(Integer.MAX_VALUE))
+	    {
+		    if(homeLauncherPackage.equals(rt.baseActivity.getPackageName()))
+		    {
+			    launcherTask = new ActivityManager.RecentTaskInfo();
+			    launcherTask.baseActivity = rt.baseActivity;
+			    launcherTask.id = rt.id;
+			    Log.d(TAG, "Launcher: " + rt.baseActivity.getPackageName() + " " + rt.id);
+		    }
+	    }
+
         Log.d(TAG, "---------------------------------------------");
         for (ActivityManager.RecentTaskInfo task : recentTasks) {
             if (task.id < 0) continue;
-            String packageName = task.baseIntent.getComponent().getPackageName();
+            String packageName = task.baseActivity.getPackageName();
+
+	        Log.d(TAG, "running through: " + packageName);
 
             // Ignoring this app
             if (Objects.equals(packageName, getPackageName())) continue;
             // Ignoring home package
             if (Objects.equals(packageName, homeLauncherPackage)) {
                 launcherTask = task;
+                Log.d(TAG, "Launcher added"+task.baseActivity.getPackageName());
                 continue;
             }
             // Ignoring any system ui activity
@@ -183,14 +216,18 @@ public class AppLogService extends Service {
         final ActivityManager mActivityManager = (ActivityManager)
                 getSystemService(Context.ACTIVITY_SERVICE);
 
+        /*
         final List<ActivityManager.RecentTaskInfo> recentTasks = mActivityManager
                 .getRecentTasks(2,
                         ActivityManager.RECENT_IGNORE_UNAVAILABLE | 4 |
                                 ActivityManager.RECENT_WITH_EXCLUDED);
 
-        String currentActivity = recentTasks.get(1).baseIntent.getComponent().getPackageName();
+        String currentActivity = recentTasks.get(1).baseActivity.getPackageName();
+		*/
 
-        Log.d(TAG, "Current activity: " + currentActivity);
+	    String currentActivity = mActivityManager.getRunningTasks(1).get(0).baseActivity.getPackageName();
+
+        Log.d(TAG, "!!!! Current activity: " + currentActivity);
 
         return currentActivity;
     }
--- app/src/main/java/in/abhi9/recentswipo/MainActivity.java
+++ app/src/main/java/in/abhi9/recentswipo/MainActivity.java
@@ -2,13 +2,29 @@ package in.abhi9.recentswipo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+	    final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		CheckBox loop = findViewById(R.id.loop);
+		loop.setChecked(prefs.getBoolean("loop", false));
+		loop.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				prefs.edit().putBoolean("loop", ((CheckBox) view).isChecked()).apply();
+			}
+		});
 
         // Starting service
         if (AppLogService.getServiceObject() == null) {
